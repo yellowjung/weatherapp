ea1366efd28e88851a2dc904bd308ec736237bf6
YoungdonJung <yeavov@gmail.com>
2022-11-09 00:04:24 +0900

    1. Make Home Screen 2. Get Current Location info

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weatherapp_starter_project/controller/global_controller.dart';
+import 'package:weatherapp_starter_project/widgets/header_widget.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // call
+  final GlobalController globalController = Get.put(GlobalController(), permanent: true);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Obx(() => globalController.checkLoading().isTrue ? const Center(
+          child: CircularProgressIndicator(),
+        ): ListView(
+          scrollDirection: Axis.vertical,
+          children: const[
+            const SizedBox(height: 20,),
+            HeaderWidget(),
+
+          ],
+        )),
+      ),
+    );
+  }
+}
